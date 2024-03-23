9cc6ab6dd407fbf1baa327377ff496d01ef39845
Achraf AYADI <115950302+MaxInformatique@users.noreply.github.com>
2024-03-23 03:04:58 +0100

    Ajouter Button

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -33,6 +33,7 @@
 			this.textBox3 = new System.Windows.Forms.TextBox();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
 			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// textBox1
@@ -73,11 +74,22 @@
 			this.comboBox1.Size = new System.Drawing.Size(121, 24);
 			this.comboBox1.TabIndex = 4;
 			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(525, 210);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(124, 44);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.comboBox1);
 			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.textBox3);
@@ -97,6 +109,7 @@
 		private System.Windows.Forms.TextBox textBox3;
 		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
